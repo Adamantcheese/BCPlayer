d2eb3fcc9c2ba626f5487ff9595dc2a635faa5e4
Adamantcheese <random123x@gmail.com>
2015-03-20 18:34:06 -0700

    moved some more stuff around, added loading buffer on first run, added index saving for later use (i.e. loading on not first run)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,86 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
-import javafx.scene.media.*;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.net.URL;
-import java.util.*;
+import java.util.Scanner;
 
 
 public class Main extends Application {
 
-    public final String BASE_DIR = System.getProperty("java.io.tmpdir") + "/BCPlayer/";
-    public final Random RANDOMIZER = new Random();
+    public static final String BASE_DIR = System.getProperty("java.io.tmpdir") + "/BCPlayer/";
+    public static final int SONG_BUFFER_SIZE = 5;
+    public static SongUtil songHelper;
+    public static File[] songFiles;
+    public static int lastIndex;
 
     @Override
     public void start (Stage primaryStage) throws Exception {
-        //Make the temp directory if it doesn't exist
-        File baseDir = new File(BASE_DIR);
-        if (!baseDir.exists()) {
-            baseDir.mkdirs();
-        }
-
-        //Load in the array of strings
-        ArrayList<String> albums = new ArrayList<String>(750);
-        File albumFile = new File(Class.forName("Main").getClassLoader().getResource("albums.csv").toURI());
-        Scanner albumScanner = new Scanner(albumFile);
-        while (albumScanner.hasNext()) {
-            String line = albumScanner.nextLine();
-            albums.add(line.substring(1, line.length() - 1));
-        }
-        albumScanner.close();
-        String[] albumURLs = new String[albums.size()];
-        albums.toArray(albumURLs);
-
         //Display the window with UI
         Parent root = FXMLLoader.load(Class.forName("Main").getClassLoader().getResource("ui.fxml"));
         primaryStage.setTitle("Bandcamp Player");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
-        //Get a random album URL
-        String URL = albumURLs[RANDOMIZER.nextInt(albumURLs.length)];
-
-        //Get all the songs in that URL
-        String[] songs = SongUtil.getSongsFromURL(URL);
-
-        //Download the file to be played
-        File tempFile = new File(BASE_DIR + "temp.mp3");
-        if (!tempFile.exists()) {
-            tempFile.createNewFile();
-            //FileUtils.copyURLToFile(new URL(songs[RANDOMIZER.nextInt(songs.length)]), tempFile);
-        }
-        FileUtils.copyURLToFile(new URL(songs[RANDOMIZER.nextInt(songs.length)]), tempFile);
-
-        //Setup the player with the testing file and play it
-        Media m = SongUtil.getMediaFromFile(tempFile);
-        MediaPlayer test = new MediaPlayer(m);
-        test.play();
+        //Start playing the first song
+        MediaPlayer player = new MediaPlayer(songHelper.getMediaFromFile(songFiles[lastIndex]));
+        player.play();
     }
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception {
+        //Make the temp directory if it doesn't exist
+        File baseDir = new File(BASE_DIR);
+        if (!baseDir.exists()) {
+            baseDir.mkdirs();
+        }
+
+        //Init the song helper
+        songHelper = new SongUtil();
+
+        //Allocate space for the song files
+        songFiles = new File[SONG_BUFFER_SIZE + 1];
+
+        //Load up 5 random songs and make a blank one for the sixth
+        //Only for initial startup
+        for (int i = 0; i <= SONG_BUFFER_SIZE; i++){
+            File tempFile = new File(BASE_DIR + "temp" + i + ".mp3");
+            if (!tempFile.exists()) {
+                tempFile.createNewFile();
+            } else {
+                songFiles[i] = tempFile;
+                continue;
+            }
+            if(i == SONG_BUFFER_SIZE) {
+                continue;
+            }
+            String albumURL = songHelper.getRandomAlbum();
+            String songURL = songHelper.getRandomSongFromURL(albumURL);
+            FileUtils.copyURLToFile(new URL(songURL), tempFile);
+        }
+
+        //Load where we left off last
+        File lastIndexFile = new File(BASE_DIR + "last.tmp");
+        if(lastIndexFile.exists()) {
+            Scanner last = new Scanner(lastIndexFile);
+            lastIndex = last.nextInt();
+            last.close();
+        } else {
+            lastIndex = 0;
+            lastIndexFile.createNewFile();
+        }
+
+        //Launch the application
         launch(args);
+
+        //Save where we are leaving off in the array
+        PrintWriter printWriter = new PrintWriter(lastIndexFile);
+        printWriter.write(Integer.toString(lastIndex));
+        printWriter.close();
     }
 }
--- src/main/java/SongUtil.java
+++ src/main/java/SongUtil.java
@@ -6,6 +6,8 @@ import org.jsoup.nodes.Element;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,13 +15,36 @@ import java.util.regex.Pattern;
  * Created by Jacob on 3/18/2015.
  */
 public class SongUtil {
-    public static String[] getSongsFromURL(String URL) {
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(URL).get();
-        } catch (IOException e) {
-            return null;
+    private Random RANDOMIZER;
+    private final String ALBUM_LIST_FILE = "albums.csv";
+    private String[] ALBUM_LIST;
+
+    public SongUtil() throws Exception {
+        //Load in the array of albums
+        ArrayList<String> albums = new ArrayList<String>(750);
+        File albumFile = new File(Class.forName("SongUtil").getClassLoader().getResource(ALBUM_LIST_FILE).toURI());
+        Scanner albumScanner = new Scanner(albumFile);
+        while (albumScanner.hasNext()) {
+            String line = albumScanner.nextLine();
+            albums.add(line.substring(1, line.length() - 1));
         }
+        albumScanner.close();
+        ALBUM_LIST = albums.toArray(new String[albums.size()]);
+
+        //Startup randomizer
+        RANDOMIZER = new Random();
+    }
+    public String getRandomSongFromURL(String URL) throws Exception {
+        String[] songs = getSongsFromURL(URL);
+        return songs[RANDOMIZER.nextInt(songs.length)];
+    }
+
+    public String getRandomAlbum() {
+        return ALBUM_LIST[RANDOMIZER.nextInt(ALBUM_LIST.length)];
+    }
+
+    private String[] getSongsFromURL(String URL) throws Exception {
+        Document doc = Jsoup.connect(URL).get();
         Element script = doc.select("script").get(8);
         Pattern p = Pattern.compile("\"mp3-128\":\".*?\"");
         Matcher match = p.matcher(script.html());
@@ -33,7 +58,7 @@ public class SongUtil {
         return songs;
     }
 
-    public static Media getMediaFromFile(File f) {
+    public Media getMediaFromFile(File f) {
         return new Media(f.toURI().toString());
     }
 }
